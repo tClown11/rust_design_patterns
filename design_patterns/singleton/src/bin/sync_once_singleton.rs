8f0afc86d5559d6a130277216897f8ef8199182c
clown <tj1211keynote@outlook.com>
2024-06-25 11:16:57 +0800

    feat: impl singleton

---
+++ design_patterns/singleton/src/bin/sync_once_singleton.rs
@@ -0,0 +1,29 @@
+use std::cell::{Ref, RefCell};
+use std::sync::{Arc, Once};
+use std::{thread, vec};
+
+type V = Option<Arc<RefCell<Vec<i32>>>>;
+
+static mut VALS: V = None;
+static INIT: Once = Once::new();
+fn main() {
+    let handle1 = thread::spawn(move || {
+        INIT.call_once(|| unsafe {
+            VALS =  Some(Arc::new(RefCell::new(vec![1, 2, 3, 4, 5, 6])));
+            println!("thread1 INIT VALS: {:?}", VALS);
+        });
+    });
+
+    let handle2 = thread::spawn(move || {
+        INIT.call_once(|| unsafe {
+            VALS = Some(Arc::new(RefCell::new(vec![1,2,3,4,5,6])));
+            println!("thread2 INIT VALS: {:?}", VALS);
+        });
+    });
+
+    handle1.join().unwrap();
+    handle2.join().unwrap();
+    println!("{:?}", unsafe {
+        VALS.take()
+    });
+}
