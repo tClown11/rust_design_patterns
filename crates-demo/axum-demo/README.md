8a0fcb5c1cf51e3cd90ba75cd943fbec71cdc43d
clown <tj1211keynote@outlook.com>
2024-07-04 09:42:06 +0800

    feat:rust adapter demo

---
+++ crates-demo/axum-demo/README.md
@@ -0,0 +1,4 @@
+# Axum 简单 demo
+
+## 参考
+[Rust crates 大巡礼：Axum](https://www.bilibili.com/video/BV1Q34y1y7DJ/?spm_id_from=pageDriver&vd_source=7c320849663a1358cbb55b300b52dfcf)
--- crates-demo/prost-demo/build.rs
+++ crates-demo/prost-demo/build.rs
@@ -8,7 +8,9 @@ fn main() {
         // . bytes(&["."])
         // 对目标字段使用 btree_map 类型
         .btree_map(&["scores"])
+        // 为数据结构增加 serialize 和 deserialize 功能
         .type_attribute(".",  "#[derive(serde::Serialize, serde::Deserialize)]")
+        // 为字段增加 serialize 和 deserialize (这里为为 data 这个字段跳过 serialize 和 deserialize 功能，因为 bytes 不支持编解码)
         .field_attribute("data", "#[serde(skip_serializing_if = \"Vec::is_empty\")]")
         .compile_protos(&["proto/person.proto"], &["."])
         .unwrap();
