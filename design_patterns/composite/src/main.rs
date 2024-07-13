e8641bb5197205ddf7a30cfaa83ee288ae594e78
clown <tj1211keynote@outlook.com>
2024-07-13 22:24:18 +0800

    chore: composite modify

--- design_patterns/composite/src/main.rs
+++ design_patterns/composite/src/main.rs
@@ -54,40 +54,20 @@ impl Component for Folder {
 
 fn main() {
     let file1 = File::new("组合模式.md");
-
     let file2 = File::new("特征.md");
-
     let file3 = File::new("struct.md");
-
-
-
     let mut folder1 = Folder::new("设计模式");
-
     folder1.add_component(file1);
 
-
-
     let mut folder2 = Folder::new("拥抱未来语言Rust");
-
     folder2.add_component(file2);
-
     folder2.add_component(file3);
-
     folder2.add_component(folder1);
 
-
-
     //      拥抱未来语言Rust
-
     //      /              |          \
-
     //   特征.md struct.md 设计模式
-
     //                                    |
-
     //                           组合模式.md
-
-
-
     folder2.search("rust");
 }
